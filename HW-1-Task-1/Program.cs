57f9af6b1d77b867c7573546164f9de8ed43e1f2
ZharkoILIEVSKI <zarko.ilievski@gmail.com>
2023-03-10 20:05:50 +0100

    прва домашна по C#

---
+++ HW-1-Task-1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Dynamic;
+using System;
+// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine();
+Console.WriteLine("***THE BEST OF THE CALCULATORS ***");
+Console.WriteLine();
+
+//Vnesuvanje na prviot broj
+Console.Write("Vnesi eden broj: ");
+double x = Convert.ToDouble(Console.ReadLine());
+
+//Vnesuvanje na vtoriot broj
+Console.Write("Vnesi drug broj: ");
+double y = Convert.ToDouble(Console.ReadLine());
+
+//Biranje na operacija
+Console.WriteLine();
+Console.WriteLine("Odberete go brojot pred operacijata sto sakate da ja napravite so broevite "+ x + " i " + y +" : ");
+
+Console.WriteLine();
+Console.WriteLine("1. SOBIRANJE     2. ODZEMANJE");
+Console.WriteLine("3. MNOZENJE      4. DELENJE");
+
+//Vnesuvanje izbor
+Console.WriteLine();
+Console.Write("Vnesete go vasiot izbor: ");
+
+string z = Console.ReadLine();
+
+Console.WriteLine();
+
+//Izvrsuvanje na kod vo odnos na izborot pogore
+if (z == "1")
+{
+    double sobiranje = x + y;
+    Console.WriteLine("**************************************************************");
+    Console.WriteLine("Operacijata SOBIRANJE go dade rezultatot: " + sobiranje);
+}
+
+if (z == "2")
+{
+    double odzemanje = x - y;
+    Console.WriteLine("**************************************************************");
+    Console.WriteLine("Operacijata ODZEMANJE go dade rezultatot: " + odzemanje);
+}
+
+if (z =="3")
+{
+    double mnozenje = x * y;
+    Console.WriteLine("**************************************************************");
+    Console.WriteLine("Operacijata MNOZENJE go dade rezultatot: " + mnozenje);
+}
+if (z =="4")
+{
+    double delenje = x / y;
+    Console.WriteLine("**************************************************************");
+    Console.WriteLine("Operacijata DELENJE go dade rezultatot: " + delenje);
+}
+
+//Blagodarnost za korisnikot
+Console.WriteLine("**************************************************************");
+Console.WriteLine("Vi blagodarime na koristenjeto!");
+Console.WriteLine("");
---
+++ HW-1-Task-2/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("");
+
+//Vnesuvanje na prviot broj
+Console.Write("Vnesi eden broj: ");
+double x = Convert.ToDouble(Console.ReadLine());
+
+//Vnesuvanje na vtoriot broj
+Console.Write("Vnesi drug broj: ");
+double y = Convert.ToDouble(Console.ReadLine());
+
+//Vnesuvanje na tret broj
+Console.Write("Vnesi tret broj: ");
+double z = Convert.ToDouble(Console.ReadLine());
+
+//Vnesuvanje na cetvrt broj
+Console.Write("Vnesi cetvrt broj: ");
+double w = Convert.ToDouble(Console.ReadLine());
+
+//Presmetka na PROSEK od broevite
+double prosek = (x + y + z + w)/4;
+Console.WriteLine("****************************************************");
+Console.WriteLine("Prosekot od broevite " + x + " " +  y + " " + z + " " + w + " e: " + prosek);
+Console.WriteLine("****************************************************");
