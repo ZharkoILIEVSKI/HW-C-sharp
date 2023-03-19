e9547839c690559c1083d5c11473923b16cdab1c
ZharkoILIEVSKI <zarko.ilievski@gmail.com>
2023-03-19 21:25:59 +0100

    домашна работа бр.4

---
+++ HW-4/HW-4-Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿
+// Task 1
+
+// Take one string from the input and print its last 5 characters.
+
+Console.Write("Vnesi zborche: ");
+string zbor = Convert.ToString(Console.ReadLine());
+
+string formatiranoZborche = zbor.Substring((zbor.Length)-5,5);
+Console.WriteLine(formatiranoZborche);
