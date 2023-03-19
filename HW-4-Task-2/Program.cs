fb50525dc1bb748b31ab295f0de59498a94e9770
ZharkoILIEVSKI <zarko.ilievski@gmail.com>
2023-03-19 22:38:30 +0100

    домашна работа бр. 4

---
+++ HW-4-Task-2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Task 2
+// Take a sentence as input and print its words.
+
+Console.Write("Vnesete recenica: ");
+string razdelenaRecenica = Console.ReadLine();
+
+string[] recenicaVoNiza = razdelenaRecenica.Split(" ");
+
+foreach (string zborce in recenicaVoNiza)
+{ 
+   Console.WriteLine(zborce);
+}
