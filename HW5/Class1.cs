d225d7372a2de8c07ac6fb82c51750f93b665fde
ZharkoILIEVSKI <zarko.ilievski@gmail.com>
2023-03-21 00:23:09 +0100

    домашна работа бр.5

---
+++ HW5/Class1.cs
@@ -0,0 +1,24 @@
+﻿namespace HW_5;
+public class Customer
+{
+    public string FirstName{get;set;}
+    public string LastName{get;set;}
+    public string CardNumber{get;set;}
+    public string PIN{get;set;}
+    public double Balance{get;set;}
+
+    public void WelcomeGreeting(string FirstName, string LastName)
+    {
+        Console.WriteLine("*** Welcome "+ FirstName + " " + LastName + " ***");
+    }
+
+    public void BalanceCheck(double Balance)
+    {
+        Console.WriteLine("The balance is: EUR" + Balance);
+    }
+
+    public void CashDeposition(double Balance)
+    {
+        Console.WriteLine("The balance is: EUR" + Balance);
+    }
+}
